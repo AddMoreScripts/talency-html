7aa45ddff5da255a997383b350742bd6a5ae1d88
addMoreScript <faraday@nxt.ru>
2023-03-14 13:37:36 +0400

    init commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+## Вёрстка сайта Talentsy
+
+На основе [Vite.js](https://vitejs.dev/)
+
+Для разработки: необходимо Node.js 14.18+
+
+`npm i`
+
+`npm run dev`
+
+для сборки:
+
+`npm run build`
---
+++ vite.config.js
@@ -0,0 +1,22 @@
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'two.html'),
+      },
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name.includes('css')) return `assets/${assetInfo.name}`;
+          return `img/${assetInfo.name}`;
+        }
+      }
+    },
+  },
+})
