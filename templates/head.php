db970ff354f74817e400ae91823300a4c9add2e1
addMoreScript <faraday@nxt.ru>
2023-04-18 20:07:42 +0400

    Раскомментировал счётчики

--- templates/head.php
+++ templates/head.php
@@ -1,7 +1,7 @@
 <?php
-  require_once __DIR__ . './../inc/helpers.php';
-  $isPromoForm = false;
-  $formClassName = '';
+require_once __DIR__ . './../inc/helpers.php';
+$isPromoForm = false;
+$formClassName = '';
 ?>
 
 <meta charset="UTF-8" />
@@ -16,28 +16,36 @@
 
 
 <!-- Google Tag Manager -->
-<!-- <script>(function (w, d, s, l, i) {
-      w[l] = w[l] || []; w[l].push({
-        'gtm.start':
-          new Date().getTime(), event: 'gtm.js'
-      }); var f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
-          'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-    })(window, document, 'script', 'dataLayer', 'GTM-WKZSGDV');</script> -->
+<script>
+  (function(w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({
+      'gtm.start': new Date().getTime(),
+      event: 'gtm.js'
+    });
+    var f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s),
+      dl = l != 'dataLayer' ? '&l=' + l : '';
+    j.async = true;
+    j.src =
+      'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+    f.parentNode.insertBefore(j, f);
+  })(window, document, 'script', 'dataLayer', 'GTM-WKZSGDV');
+</script>
 <!-- End Google Tag Manager -->
 
 
-<!-- <script src="https://app.getreview.io/tags/mugUGuLoIpZqq1qD/sdk.js" async></script> -->
+<script src="https://app.getreview.io/tags/mugUGuLoIpZqq1qD/sdk.js" async></script>
 
 <!-- Pixel -->
-<!-- <script type="text/javascript">
-    (function (d, w) {
-      var n = d.getElementsByTagName("script")[0],
-        s = d.createElement("script");
-      s.type = "text/javascript";
-      s.async = true;
-      s.src = "https://qoopler.ru/index.php?ref=" + d.referrer + "&page=" + encodeURIComponent(w.location.href);
-      n.parentNode.insertBefore(s, n);
-    })(document, window);
-  </script> -->
+<script type="text/javascript">
+  (function(d, w) {
+    var n = d.getElementsByTagName("script")[0],
+      s = d.createElement("script");
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = "https://qoopler.ru/index.php?ref=" + d.referrer + "&page=" + encodeURIComponent(w.location.href);
+    n.parentNode.insertBefore(s, n);
+  })(document, window);
+</script>
 <!-- /Pixel -->
